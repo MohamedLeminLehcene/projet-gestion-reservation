138335be38779d37a57982faa4192eeeaabb8ec2
Lehcene <lehcenemohamedlemine@gmail.com>
2023-12-30 01:04:10 +0100

    n c

--- reservation-service/src/main/java/org/sdia/reservationservice/dto/PersonneResponseDTO.java
+++ reservation-service/src/main/java/org/sdia/reservationservice/dto/PersonneResponseDTO.java
@@ -1,12 +1,19 @@
 package org.sdia.reservationservice.dto;
 
+
 import lombok.*;
+import org.sdia.reservationservice.entities.Reservation;
 
-import java.awt.*;
+import java.util.List;
 
-@Getter @Setter @AllArgsConstructor @NoArgsConstructor @Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PersonneResponseDTO {
     private Long id;
     private String nom;
     private String email;
+    private List<Reservation> responseDTOList;
 }
