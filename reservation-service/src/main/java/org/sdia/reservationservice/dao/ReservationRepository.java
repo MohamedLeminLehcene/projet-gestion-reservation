f7a45286359c15792c3e5fa81825c7c360056317
Lehcene <lehcenemohamedlemine@gmail.com>
2023-12-30 01:03:44 +0100

    n c

--- reservation-service/src/main/java/org/sdia/reservationservice/dao/ReservationRepository.java
+++ reservation-service/src/main/java/org/sdia/reservationservice/dao/ReservationRepository.java
@@ -4,6 +4,9 @@ import org.sdia.reservationservice.entities.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+    List<Reservation> findByPersonneId(Long personneId);
 }
