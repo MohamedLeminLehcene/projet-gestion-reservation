0e73683572a8d9cb8a49558b67373399b07fffee
Lehcene <lehcenemohamedlemine@gmail.com>
2023-12-30 01:05:27 +0100

    n c

--- reservation-service/src/main/java/org/sdia/reservationservice/service/PersonneService.java
+++ reservation-service/src/main/java/org/sdia/reservationservice/service/PersonneService.java
@@ -4,6 +4,8 @@ package org.sdia.reservationservice.service;
 import org.sdia.reservationservice.dto.PersonneRequestDTO;
 import org.sdia.reservationservice.dto.PersonneResponseDTO;
 import org.sdia.reservationservice.dto.ReservationResponseDTO;
+import org.sdia.reservationservice.entities.Reservation;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -15,6 +17,11 @@ public interface PersonneService {
 
     public List<PersonneResponseDTO> allPeronnes();
 
+
     public void deletePersonne(Long id);
+
+    public PersonneResponseDTO personneById(Long id);
+
+
 
 }
