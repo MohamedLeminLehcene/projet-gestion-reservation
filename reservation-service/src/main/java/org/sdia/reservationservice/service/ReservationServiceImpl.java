0a7499e401290af7a49b14508fdcc552127c5d54
Lehcene <lehcenemohamedlemine@gmail.com>
2023-12-30 01:06:33 +0100

    n c

--- reservation-service/src/main/java/org/sdia/reservationservice/service/ReservationServiceImpl.java
+++ reservation-service/src/main/java/org/sdia/reservationservice/service/ReservationServiceImpl.java
@@ -6,10 +6,13 @@ import org.sdia.reservationservice.dao.ReservationRepository;
 import org.sdia.reservationservice.dto.PersonneResponseDTO;
 import org.sdia.reservationservice.dto.ReservationRequestDTO;
 import org.sdia.reservationservice.dto.ReservationResponseDTO;
+import org.sdia.reservationservice.dto.RessourceResponseDTO;
 import org.sdia.reservationservice.entities.Personne;
 import org.sdia.reservationservice.entities.Reservation;
 import org.sdia.reservationservice.mappers.ReservationMapper;
+import org.sdia.reservationservice.model.Ressource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Date;
 import java.util.List;
@@ -19,11 +22,15 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class ReservationServiceImpl implements ReservationService{
 
+
     private final ReservationRepository reservationRepository;
 
     private final ReservationMapper reservationMapper;
 
     private final PersonneRepository personneRepository;
+
+    private final RessourceRestClientController ressourceRestClientController;
+
 
     @Override
     public ReservationResponseDTO addReservation(ReservationRequestDTO reservationRequestDTO) {
@@ -34,7 +41,7 @@ public class ReservationServiceImpl implements ReservationService{
                 .date(new Date())
                 .dure(reservationRequestDTO.getDure())
                 .ressourceId(reservationRequestDTO.getRessourceId())
-                .personne(personneRepository.findById(reservationRequestDTO.getPerssoneId()).get())
+                .personne(personneRepository.findById(reservationRequestDTO.getPersonneId()).get())
                 .build();
 
        Reservation savedReservation = reservationRepository.save(reservation);
@@ -76,4 +83,61 @@ public class ReservationServiceImpl implements ReservationService{
     public void deleteReservation(Long id) {
         reservationRepository.deleteById(id);
     }
+
+    @Override
+    public List<Reservation> getReservationsByPersonId(Long personId) {
+
+        List<Reservation> reservationList = reservationRepository.findByPersonneId(personId);
+
+        reservationList.forEach(reservation -> {
+
+            RessourceResponseDTO ressourceResponseDTO = ressourceRestClientController.ressourcesById(reservation.getRessourceId());
+
+            Ressource ressource = new Ressource();
+            ressource.setId(ressourceResponseDTO.getId());
+
+            ressource.setNom(ressourceResponseDTO.getNom());
+
+            ressource.setRessourceType(ressourceResponseDTO.getRessourceType());
+
+            reservation.setRessource(ressource);
+
+        });
+
+        return reservationList;
+
+    }
+
+
+
+
+    @Override
+    public ReservationResponseDTO reservationByIdDetail(Long id) {
+
+        Reservation reservation = reservationRepository.findById(id).orElseThrow();
+
+        RessourceResponseDTO ressourceResponseDTO = ressourceRestClientController.ressourcesById(reservation.getRessourceId());
+
+        Personne personne = personneRepository.findById(reservation.getPersonne().getId()).get();
+
+
+        Ressource ressource = new Ressource();
+
+        ressource.setId(ressourceResponseDTO.getId());
+
+        ressource.setNom(ressourceResponseDTO.getNom());
+
+        ressource.setRessourceType(ressourceResponseDTO.getRessourceType());
+
+        reservation.setRessource(ressource);
+
+        reservation.setPersonne(personne);
+
+        ReservationResponseDTO reservationResponseDTO = reservationMapper.fromReservation(reservation);
+
+        return reservationResponseDTO;
+    }
+
+
 }
+
