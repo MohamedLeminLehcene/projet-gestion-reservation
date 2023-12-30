a62088c3b903737865ebb2699e4ffe8b9ec47c82
Lehcene <lehcenemohamedlemine@gmail.com>
2023-12-30 01:06:55 +0100

    n c

--- reservation-service/src/main/java/org/sdia/reservationservice/web/PersonneRestController.java
+++ reservation-service/src/main/java/org/sdia/reservationservice/web/PersonneRestController.java
@@ -1,20 +1,30 @@
 package org.sdia.reservationservice.web;
 
 import lombok.AllArgsConstructor;
+import org.sdia.reservationservice.dao.PersonneRepository;
+import org.sdia.reservationservice.dao.ReservationRepository;
 import org.sdia.reservationservice.dto.PersonneRequestDTO;
 import org.sdia.reservationservice.dto.PersonneResponseDTO;
 import org.sdia.reservationservice.dto.ReservationResponseDTO;
+import org.sdia.reservationservice.entities.Personne;
+import org.sdia.reservationservice.entities.Reservation;
 import org.sdia.reservationservice.mappers.PersonneMapper;
 import org.sdia.reservationservice.service.PersonneService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api")
 public class PersonneRestController {
 
     private final PersonneService personneService;
+
+    private final PersonneRepository personneRepository;
+
+    private final ReservationRepository reservationRepository;
 
     private final PersonneMapper personneMapper;
 
@@ -34,9 +44,16 @@ public class PersonneRestController {
         return personneService.allPeronnes();
     }
 
-    @DeleteMapping("/peronnes/{id}")
+
+    @DeleteMapping("/peronnes/delete/{id}")
     public void deletePersonne(@PathVariable Long id){
         personneService.deletePersonne(id);
     }
+
+    @GetMapping("/personnes/ById/{id}")
+    public PersonneResponseDTO personneById(@PathVariable Long id){
+     return  personneService.personneById(id);
+    }
+
 }
 
