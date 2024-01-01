22ec7d496dc1878af6ccd3e970978fd26379845c
Lehcene <lehcenemohamedlemine@gmail.com>
2024-01-01 22:37:03 +0100

    n c

--- reservation-service/src/main/java/org/sdia/reservationservice/entities/Reservation.java
+++ reservation-service/src/main/java/org/sdia/reservationservice/entities/Reservation.java
@@ -1,6 +1,7 @@
 package org.sdia.reservationservice.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.*;
 import lombok.*;
 import org.sdia.reservationservice.model.Ressource;
@@ -10,7 +11,8 @@ import java.util.Date;
 @Entity
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor @Builder
 public class Reservation {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nom;
     private String contexte;
